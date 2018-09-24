a351dcbbb598111f1b431cf0c3496f6532d91072
jasper55 <32734771+jasper55@users.noreply.github.com>
2018-09-24 12:17:48 +0200

    fix bugs clean code

--- app/src/main/java/com/example/app/jasper/voicetotext/MainActivity.java
+++ app/src/main/java/com/example/app/jasper/voicetotext/MainActivity.java
@@ -37,100 +37,40 @@ public class MainActivity extends AppCompatActivity {
     public static final String recordAudioPermission = Manifest.permission.RECORD_AUDIO;
     public static final String internetPermission = Manifest.permission.INTERNET;
 
+    String[] langList = {"GERMAN","ENGLISH","FRENCH"};
+    String language = "GERMAN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setIcon(getDrawable(R.drawable.ic_launcher_background));
+        //toolbar.setNavigationIcon(R.drawable.ic_launcher_foreground);
+        //toolbar.setTitle(R.string.app_name);
+        //getSupportActionBar().setIcon(getDrawable(R.drawable.ic_launcher_foreground));
+        //getSupportActionBar().setTitle(R.string.app_name);
 
-        textField = findViewById(R.id.textContainer);
-        copyTextButton = findViewById(R.id.copyButton);
+        textField = (EditText) findViewById(R.id.textContainer);
+        copyTextButton = (Button)findViewById(R.id.copyButton);
 
         showPermission();
         requestPermission();
-    }
 
-
-    //Sprache wählen
-    String[] langList = {"GERMAN","ENGLISH","FRENCH"};
-    String language = "GERMAN";
-    private String chooseLanguage (String[] langlist, int i){
-        String chosenLanguage = langlist[i];
-        this.language = chosenLanguage;
-        return language;
-    }
-    private Locale setLanguage(String language){
-        Locale lang = new Locale(language);
-        return lang;
-    }
-    //---------------------------
-
-
-    public void onClickCopy(View view){
-        ClipboardManager clipboard = (ClipboardManager)this.getSystemService(this.CLIPBOARD_SERVICE);
-        String input = textField.getText().toString();
-        ClipData clipData = ClipData.newPlainText("input",input);
-        clipboard.setPrimaryClip(clipData);
-    }
-    public void onClickRecord(View view){
-
-        if(!isRecording) {
-
-        //da vom System etwas zurück an die App gegeben werden muss,
-        // muss die Methode startActivityFOrResult aufgerufen werden
-        // requestCode kann frei ausgesucht werden
-        try {
-            Intent recordIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            // Art und Weise, welchen Text man als Rückgabe erhällt
-            recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
-            //recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-            recordIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 100);
-            //recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, setLanguage(language));
-            recordIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Jetzt Sprechen");
-
-            startActivityForResult(recordIntent, REQ_CODE_SPEECH_INPUT);
-            Toast.makeText(this, "Activity started", Toast.LENGTH_SHORT).show();
+    }   // onCreate()
 
 
 
-        } catch(ActivityNotFoundException e) {
-            String appPackageName = "com.google.android.googlequicksearchbox";
-            Toast.makeText(this, "Activity not found", Toast.LENGTH_LONG).show();
-            try {
-
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-                Toast.makeText(this, "Action_View", Toast.LENGTH_LONG).show();
-
-            } catch (android.content.ActivityNotFoundException anfe) {
-                Toast.makeText(this, "anfe -- Activity not found", Toast.LENGTH_LONG).show();
-
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
-            }
-        }
-        } else {
-            isRecording = false;
-        }
-    }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
-        //Toast.makeText(this,resultCode ,Toast.LENGTH_LONG).show();
         if(requestCode==REQ_CODE_SPEECH_INPUT)
-
-            //Toast.makeText(this, resultCode,Toast.LENGTH_LONG).show();
         {
             if(resultCode == RESULT_OK && data != null){
-                //Toast.makeText(this, "requestCode funktioniert, Data !=NULL",Toast.LENGTH_LONG).show();
                 ArrayList result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                //Toast.makeText(this, (CharSequence) result.get(0), Toast.LENGTH_LONG).show();
                 textField.setText((CharSequence) result.get(0));
             }
         }
@@ -152,6 +92,55 @@ public class MainActivity extends AppCompatActivity {
                 }
         }
     }
+
+    public void onClickCopy(View view){
+        ClipboardManager clipboard = (ClipboardManager)this.getSystemService(this.CLIPBOARD_SERVICE);
+        String input = textField.getText().toString();
+        ClipData clipData = ClipData.newPlainText("input",input);
+        clipboard.setPrimaryClip(clipData);
+    }
+    public void onClickRecord(View view){
+
+        if(!isRecording) {
+
+            //da vom System etwas zurück an die App gegeben werden muss,
+            // muss die Methode startActivityFOrResult aufgerufen werden
+            // requestCode kann frei ausgesucht werden
+            try {
+                Intent recordIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                // Art und Weise, welchen Text man als Rückgabe erhällt
+                recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+                //recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+                recordIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 100);
+                //recordIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, setLanguage(language));
+                recordIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Jetzt Sprechen");
+
+                startActivityForResult(recordIntent, REQ_CODE_SPEECH_INPUT);
+                Toast.makeText(this, "Activity started", Toast.LENGTH_SHORT).show();
+
+
+
+            } catch(ActivityNotFoundException e) {
+                String appPackageName = "com.google.android.googlequicksearchbox";
+                Toast.makeText(this, "Activity not found", Toast.LENGTH_LONG).show();
+                try {
+
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                    Toast.makeText(this, "Action_View", Toast.LENGTH_LONG).show();
+
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    Toast.makeText(this, "anfe -- Activity not found", Toast.LENGTH_LONG).show();
+
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                }
+            }
+        } else {
+            isRecording = false;
+        }
+    }
+
 
     private void requestPermission() {
         int permissionCheck = ContextCompat.checkSelfPermission(this, recordAudioPermission);
@@ -176,6 +165,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Sprache wählen
+
+    private String chooseLanguage (String[] langlist, int i){
+        String chosenLanguage = langlist[i];
+        this.language = chosenLanguage;
+        return language;
+    }
+    private Locale setLanguage(String language){
+        Locale lang = new Locale(language);
+        return lang;
+    }
+    //---------------------------
 
 
 
@@ -183,7 +184,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
